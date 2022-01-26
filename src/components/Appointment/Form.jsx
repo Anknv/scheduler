4360daab0196571636b230a978ede09e9468c515
Anknv <itsannakonovalova@gmail.com>
2022-01-25 21:42:16 -0500

    write tests

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -10,16 +10,29 @@ export default function Form(props) {
 
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(value || null);
+  const [error, setError] = useState("");
 
 
   const reset = () => {
     setStudent("");
+    setError("");
     setInterviewer(null);
   }
   
   const cancel = (event) => {
-    //reset();
+    reset();
     onCancel();
+  }
+
+  function validate() {
+
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    
+    setError("");
+    onSave(student, interviewer);
   }
 
 
@@ -29,12 +42,14 @@ export default function Form(props) {
         <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
+            data-testid="student-name-input"
             name="name"
             value={student}
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
           />
+          <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList
           interviewers={interviewers}
@@ -45,7 +60,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={event => onSave(student, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -2,22 +2,29 @@ import React from "react";
 import classNames from "classnames";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   const {interviewers, onChange, value } = props;
   const parsedInterviewerListItem = interviewers.map(oneInterviewer => 
     <InterviewerListItem
-      key={oneInterviewer.id} 
-      selected={oneInterviewer.id === value}
-      setInterviewer={() => onChange(oneInterviewer.id)}
-      {...oneInterviewer}
+    key={oneInterviewer.id} 
+    selected={oneInterviewer.id === value}
+    setInterviewer={() => onChange(oneInterviewer.id)}
+    {...oneInterviewer}
     />
-  );
-  
-  return (
-    <section className="interviewers">
+    );
+    
+    return (
+      <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
       <ul className="interviewers__list">{ parsedInterviewerListItem }</ul>
     </section>   
-    );
-  }
+  );
+}
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+export default InterviewerList;
